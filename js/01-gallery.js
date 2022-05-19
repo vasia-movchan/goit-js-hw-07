6db14c45461b7b6dfc3b50dd97b19c0c3727895b
Vasiliy Movchan <movchan.vasia@gmail.com>
2022-05-19 20:46:19 +0300

    create gallery markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galeryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", galeryMarkup);
