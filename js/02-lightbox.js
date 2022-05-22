5fb75361693bb5b89d156c5454ca7cafe7b4819d
Vasiliy Movchan <movchan.vasia@gmail.com>
2022-05-22 10:07:42 +0300

    task-02 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const createGaleryItemMarkup = (preview, original, description) =>
+  `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+
+const galeryMarkup = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    acc + createGaleryItemMarkup(preview, original, description),
+  ""
+);
+
+gallery.insertAdjacentHTML("beforeend", galeryMarkup);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
